8ebdfc4603cbe6fe4a412c4197e19b17cc724805
Tasnim Alam <tasnimalamcse@gmail.com>
2021-02-28 17:00:00 +0600

    Add gradient color control submodule

--- util/gradient-color-controller/constants.js
+++
@@ -1,9 +0,0 @@
-export const GRADIENT_TYPE = [
-  { label: "Linear", value: "linear" },
-  { label: "Radial", value: "radial" }
-];
-
-export const RADIAL_TYPES = [
-  { label: "Ellipse", value: "ellipse" },
-  { label: "Circle", value: "circle" }
-];
