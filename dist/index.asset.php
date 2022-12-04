d57697a3a472bff3fe515f4fe497372c2f71f1b0
Monir <fencemonir@gmail.com>
2022-12-04 17:52:22 +0600

    update from eb

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0c77b0d360f1e946aa03');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e132f1311e45850031b6');
